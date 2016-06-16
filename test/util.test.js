d34d661a7229f094f9050143fe54affefdabebde
Matt Broadstone <mbroadst@gmail.com>
2016-06-16 13:45:05 -0400

    feat(inject): auto inject an `id` field if no `pk` specified

--- test/util.test.js
+++ test/util.test.js
@@ -27,5 +27,28 @@ describe('util', function() {
         expect(actual).to.eql(t.expected);
       });
     });
-  });
-});
+
+    it('should inject an `id` property if no `pk` is specified', () => {
+      let schema = { type: 'object', properties: { name: { type: 'string' } } };
+      let result = util.inject(schema);
+      expect(result).to.eql({
+        type: 'object',
+        properties: {
+          id: { type: 'string', acceptTerms: true },
+          name: { type: 'string', acceptTerms: true }
+        }
+      });
+    });
+
+    it('should not inject an `id` property if `pk` is specified', () => {
+      let schema = { type: 'object', properties: { name: { type: 'string' } } };
+      let result = util.inject(schema, { pk: 'name' });
+      expect(result).to.eql({
+        type: 'object',
+        properties: {
+          name: { type: 'string', acceptTerms: true }
+        }
+      });
+    });
+  }); // inject
+}); // util
